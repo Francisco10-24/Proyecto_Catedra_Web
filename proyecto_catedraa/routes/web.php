4194765b29a5d0dbbdd5f24a22e864a3049a4f1d
Francisco10-24 <frauda003@gmail.com>
2026-02-10 22:25:57 -0600

    Update web.php

--- proyecto_catedraa/routes/web.php
+++ proyecto_catedraa/routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
+
+
+
 Route::get('/', function () {
     return Inertia::render('welcome', [
         'canRegister' => Features::enabled(Features::registration()),
